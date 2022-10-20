52b253c0d4f004b53a68502d87ad73aefa4fc4e5
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-10-20 22:56:16 +0300

    DZ-1

--- Z_1/Program.cs
+++ Z_1/Program.cs
@@ -3,57 +3,28 @@
 //количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
-int [] array = GetArray(4, 100, 999);
+int[] array = GetArray(4, 100, 999);
 int CountEvenNumbers = GetCountEvenNumbers(array);
 Console.Write(String.Join(" ", array));
 Console.Write($" -> {CountEvenNumbers}");
 
 /////
-int [] GetArray(int size, int minValue, int maxValue)
+int[] GetArray(int size, int minValue, int maxValue)
 {
-    int [] res = new int[size];
+    int[] res = new int[size];
     for (int i = 0; i < size; i++)
     {
-        res[i] = new Random().Next(minValue, maxValue+1);
+        res[i] = new Random().Next(minValue, maxValue + 1);
     }
     return res;
 }
 /////
-int GetCountEvenNumbers(int []arr)
+int GetCountEvenNumbers(int[] arr)
 {
     int CountEvenNumbers = 0;
     foreach (int el in arr)
     {
-        if (el % 2 ==0) CountEvenNumbers +=1;
+        if (el % 2 == 0) CountEvenNumbers += 1;
     }
-    return CountEvenNumbers;        
+    return CountEvenNumbers;
 }
-// //31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
-// //Найдите сумму отрицательных и положительных элементов массива.
-// Console.Clear();
-// int[] array = GetArray(4, 100, 999);
-// Console.WriteLine(String.Join(" ", array));
-// int positiveSum = GetPositiveSum(array);
-// Console.WriteLine($"Positive sum = {positiveSum}");
-// //////////////////////////////////////////////////////////////////////////////////
-// // Возвращает массив из size элементов,
-// // заполненный случайными числами из промежутка [minValue, maxValue]
-// int[] GetArray(int size, int minValue, int maxValue)
-// {
-//     int[] res = new int[size];
-//     for (int i = 0; i < size; i++)
-//     {
-//         res[i] = new Random().Next(minValue, maxValue + 1);
-//     }
-//     return res;
-// }
-// // Возвращает сумму положительных чисел массива arr
-// int GetPositiveSum(int[] arr)
-// {
-//     int positiveSum = 0;
-//     foreach (int el in arr)
-//     {
-//         if (el%2==0) positiveSum += 1;
-//     }
-//     return positiveSum;
-// }
