93295745365cbc6d404f18de8d36fdf293b2580b
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-10-21 19:48:21 +0300

    DZ-3

--- Z_3/Program.cs
+++ Z_3/Program.cs
@@ -2,30 +2,36 @@
 // и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
    
-double[] array = new double[] { 3, 7, 22, 2, 78 };
-Console.Write(String.Join(",", array));
-Console.Write($"-> {GetMaxValue(array) - GetMinValue(array)}");
+double[] array = GetArray(4);
+Console.WriteLine($"Массив: {String.Join(", ", array)}");
 
-double GetMinValue(double[] array)
+double minVaue = array[0], maxVaue = array[0];
+
+for (int i = 0; i < array.Length; i++)
 {
-    double result = 0;
-
-    for (int i = 0; i < array.Length; i++)
+    if (minVaue > array[i])
     {
-        if (array[i] < result) result = array[i];
+        minVaue = array[i];
     }
 
-    return result;
+    if (maxVaue < array[i])
+    {
+        maxVaue = array[i];
+    }
 }
 
-double GetMaxValue(double[] array)
-{
-    double result = 0;
+Console.WriteLine($"Минимальное число: {minVaue}");
+Console.WriteLine($"Максимальное число: {maxVaue}");
+Console.WriteLine($"Разница между минимальным и максимальным числом: {maxVaue - minVaue}");
 
-    for (int i = 0; i < array.Length; i++)
+double[] GetArray(int size)
+{
+    double[] res = new double[size];
+
+    for (int i = 0; i < size; i++)
     {
-        if (array[i] > result) result = array[i];
+        res[i] = new Random().NextDouble() * 100;
     }
 
-    return result;
+    return res;
 }
