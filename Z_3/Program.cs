cca85912f39d1ae4870f307f7208e3bb35dcb012
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-10-21 19:18:44 +0300

    DZ-3

---
+++ Z_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным 
+// и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+   
+double[] array = new double[] { 3, 7, 22, 2, 78 };
+Console.Write(String.Join(",", array));
+Console.Write($"-> {GetMaxValue(array) - GetMinValue(array)}");
+
+double GetMinValue(double[] array)
+{
+    double result = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < result) result = array[i];
+    }
+
+    return result;
+}
+
+double GetMaxValue(double[] array)
+{
+    double result = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > result) result = array[i];
+    }
+
+    return result;
+}
